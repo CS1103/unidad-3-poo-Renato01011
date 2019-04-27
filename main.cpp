5fbfb178e09a2f5e5b40a45edea7ebd30707b3aa
Renato01011 <40170602+Renato01011@users.noreply.github.com>
2019-04-26 21:57:56 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,15 +4,6 @@
 #include "catch.hpp"
 using namespace std;
 
-TEST_CASE("Calcular Potencial Electrico", "[PotencialElectrico]") {
-
-    string Result = "";
-    Simulador Sim1(10,10,5,4,2,2,0.000001,4,4,0.000001,6,6,0.000001,8,8,0.000001);
-    Result = Sim1.CalcularPotencialElectrico();
-    REQUIRE(Result == " 2.500000, 2.000000, 39558728914.374649 5.000000, 4.000000, 28995958971.829441 7.500000, 6.000000, 19093819357.567024 10.000000, 8.000000, 13396294341.701462 12.500000, 10.000000, 10142134191.621128");
-
-}
-
 /*
 int main() {
 
@@ -23,3 +14,16 @@ int main() {
     return 0;
 }
 */
+
+SCENARIO("Potencial Electrico") {
+    GIVEN("Calcular Potencial Electrico") {
+        WHEN("Cuatro cargas en posiciones 2,2 4,4 6,6 8,8 de 1 nanoCoulumb cada una") {
+            string Result = "";
+            Simulador Sim1(10,10,5,4,2,2,0.000001,4,4,0.000001,6,6,0.000001,8,8,0.000001);
+            Result = Sim1.CalcularPotencialElectrico();
+            THEN("Los potenciales son 3.95587e+010, 2.8996e+010, 1.90938e+010, 1.33963e+010, 1.01421e+010") {
+                REQUIRE(Result == " 2.500000, 2.000000, 39558728914.374649 5.000000, 4.000000, 28995958971.829445 7.500000, 6.000000, 19093819357.567024 10.000000, 8.000000, 13396294341.701464 12.500000, 10.000000, 10142134191.621128");
+            }
+        }
+    }
+}
